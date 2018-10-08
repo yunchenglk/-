68f39ea5ceac56c6032303b9cd6651e0f3d5c9d5
likui <likui@likui.com>
2018-10-09 00:17:51 +0800

    1

---
+++ Learun.Framework.Module/Learun.Log/LogLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+namespace Learun.Loger
+{
+    /// <summary>
+    /// 版 本 V2.3 辰星软件开发框架
+    /// Copyright (c) 2013-2020 山西辰星软件科技有限公司
+    /// 创建人：System
+    /// 日 期：2017.03.04
+    /// 描 述：日志级别
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// 错误
+        /// </summary>
+        [Description("错误")]
+        Error,
+        /// <summary>
+        /// 警告
+        /// </summary>
+        [Description("警告")]
+        Warning,
+        /// <summary>
+        /// 信息
+        /// </summary>
+        [Description("信息")]
+        Info,
+        /// <summary>
+        /// 调试
+        /// </summary>
+        [Description("调试")]
+        Debug
+    }
+}
