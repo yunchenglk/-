f431b770b4b2b6ad332065ec3973d0cc57b0ae60
likui <likui@likui.com>
2018-10-08 16:43:39 +0800

    1

---
+++ Learun.Application.IMServer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Learun.Application.IMServer
+{
+    /// <summary>
+    /// 版 本 V2.3 辰星软件开发框架
+    /// Copyright (c) 2013-2018 上海力软信息技术有限公司
+    /// 创建人：力软-框架开发组
+    /// 日 期：2017.04.01
+    /// 描 述：程序开始入口
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Startup.Start();
+        }
+    }
+}
---
+++ Learun.Application.Web/Areas/LR_OAModule/Controllers/SignetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Learun.Application.Web.Areas.LR_OAModule.Controllers
+{
+    /// <summary>
+    /// 版 本 V2.3 辰星软件开发框架
+    /// Copyright (c) 2013-2018 上海力软信息技术有限公司
+    /// 创建人：力软-框架开发组
+    /// 日 期：2017.07.17
+    /// 描 述：电子签章
+    /// </summary>
+    public class SignetController : MvcControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
