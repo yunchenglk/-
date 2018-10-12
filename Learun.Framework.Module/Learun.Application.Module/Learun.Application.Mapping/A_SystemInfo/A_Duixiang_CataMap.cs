e421edd9f4fa47edf8c0900e63c62304d38756ae
wxy <wxy@wxy.com>
2018-10-12 18:44:55 +0800

    333

---
+++ Learun.Framework.Module/Learun.Application.Module/Learun.Application.Mapping/A_SystemInfo/A_Duixiang_CataMap.cs
@@ -0,0 +1,29 @@
+﻿using Learun.Application.TwoDevelopment.A_SystemInfo;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Learun.Application.Mapping
+{
+    /// <summary>
+    /// 版 本V2.3 辰星软件开发框架
+    /// Copyright (c) 2013-2018 山西辰星昇软件科技有限公司
+    /// 创 建：超级管理员
+    /// 日 期：2018-10-12 18:43
+    /// 描 述：业务对象类别字典管理
+    /// </summary>
+    public class A_Duixiang_CataMap : EntityTypeConfiguration<A_Duixiang_CataEntity>
+    {
+        public A_Duixiang_CataMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("A_DUIXIANG_CATA");
+            //主键
+            this.HasKey(t => t.ID);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
---
+++ Learun.Framework.Module/Learun.Application.Module/Learun.Application.TwoDevelopment/A_SystemInfo/A_Duixiang_Cata/A_Duixiang_CataIBLL.cs
@@ -0,0 +1,49 @@
+﻿using Learun.Util;
+using System.Data;
+using System.Collections.Generic;
+
+namespace Learun.Application.TwoDevelopment.A_SystemInfo
+{
+    /// <summary>
+    /// 版 本V2.3 辰星软件开发框架
+    /// Copyright (c) 2013-2018 山西辰星昇软件科技有限公司
+    /// 创 建：超级管理员
+    /// 日 期：2018-10-12 18:43
+    /// 描 述：业务对象类别字典管理
+    /// </summary>
+    public interface A_Duixiang_CataIBLL
+    {
+        #region 获取数据
+
+        /// <summary>
+        /// 获取页面显示列表数据
+        /// <summary>
+        /// <param name="queryJson">查询参数</param>
+        /// <returns></returns>
+        IEnumerable<A_Duixiang_CataEntity> GetPageList(Pagination pagination, string queryJson);
+        /// <summary>
+        /// 获取A_Duixiang_Cata表实体数据
+        /// <param name="keyValue">主键</param>
+        /// <summary>
+        /// <returns></returns>
+        A_Duixiang_CataEntity GetA_Duixiang_CataEntity(string keyValue);
+        #endregion
+
+        #region 提交数据
+
+        /// <summary>
+        /// 删除实体数据
+        /// <param name="keyValue">主键</param>
+        /// <summary>
+        /// <returns></returns>
+        void DeleteEntity(string keyValue);
+        /// <summary>
+        /// 保存实体数据（新增、修改）
+        /// <param name="keyValue">主键</param>
+        /// <summary>
+        /// <returns></returns>
+        void SaveEntity(string keyValue, A_Duixiang_CataEntity entity);
+        #endregion
+
+    }
+}
