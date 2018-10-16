74d2a1c2c948391658aefc9adb6e35ccf82943ed
likui <likui@likui.com>
2018-10-17 04:59:45 +0800

    1

--- Learun.Application.Web/Views/LR_Content/script/lr-admin.js
+++ Learun.Application.Web/Views/LR_Content/script/lr-admin.js
@@ -154,8 +154,7 @@ var loaddfimg;
         // 皮肤主题设置
         uitheme: function () {
          
-            var uitheme = top.$.cookie('Learn_ADMS_V6.1_UItheme') || '3';
-            alert(uitheme);
+            var uitheme = top.$.cookie('Learn_ADMS_V6.1_UItheme') || '0';
             var $setting = $('<div class="lr-theme-setting" style="display:none"></div>');
             var $btn = $('<button class="btn btn-default"><i class="fa fa-spin fa-gear"></i></button>');
             var _html = '<div class="panel-heading">界面风格</div>';
