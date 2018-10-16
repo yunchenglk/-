759e3011398efca8b944e77c03aeb0e594a28104
likui <likui@likui.com>
2018-10-17 03:45:41 +0800

    111

--- CS/Default.Designer.cs
+++ CS/Default.Designer.cs
@@ -49,6 +49,7 @@
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.webBrowser1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Default";
             this.Text = "辰星HIS平台";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
